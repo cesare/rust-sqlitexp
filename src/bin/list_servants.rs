e315dab9d0c3ecdb14f5a7da415516b7726f1caa
SAWADA Tadashi <cesare@mayverse.jp>
2021-03-29 23:04:19 +0900

    Show servants

--- src/bin/list_servants.rs
+++ src/bin/list_servants.rs
@@ -1,7 +1,7 @@
 use std::str::FromStr;
 
 use anyhow::Result;
-use sqlx::{Connection, ConnectOptions, Executor};
+use sqlx::{Connection, ConnectOptions};
 use sqlx::sqlite::SqliteConnectOptions;
 use structopt::StructOpt;
 use tokio;
@@ -13,6 +13,13 @@ struct Options {
     url: String,
 }
 
+#[derive(Debug, sqlx::FromRow)]
+struct Servant {
+    id: i64,
+    name: String,
+    class_name: String,
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
     let options = Options::from_args();
@@ -20,9 +27,11 @@ async fn main() -> Result<()> {
     let mut connection = SqliteConnectOptions::from_str(&options.url)?
         .create_if_missing(true)
         .connect().await?;
-    let query = sqlx::query("select id, name, class_name from servants");
-    let results = connection.execute(query).await?;
-    println!("{:?}", results);
+    let query = sqlx::query_as::<_, Servant>("select id, name, class_name from servants");
+    let results = query.fetch_all(&mut connection).await?;
+    for servant in results {
+        println!("{:?}", servant);
+    }
     connection.close().await?;
 
     Ok(())
